e1fe2a203df56bb6e4f39e220b53b0d963729cf8
danielkhavin <115034985+Dannykhvn@users.noreply.github.com>
2022-11-15 10:42:08 -0800

    Worked with appending list items into a function and list

--- scripts/script.js
+++ scripts/script.js
@@ -5,8 +5,26 @@
 // let greet = function(name){
 //     console.log(`Hello ${name}`);
 // };
-let greet = (name, time) => {
-    console.log(`Hello ${name} the time is ${time}`);
+// let greet = (name, time) => {
+//     console.log(`Hello ${name} the time is ${time}`);
+// }
+// var newtime = new Date();
+// var print = greet(prompt("Please enter your name: "), newtime.getHours() + ":" + newtime.getMinutes() + ":" + newtime.getSeconds());
+
+
+const unorderedlistelement = document.querySelector(".shopping");
+function populateList (list){
+    for (let i = 0 ; i < list.length ; i++){
+        console.log(list[i]);
+        const listItemElement = document.createElement("li");
+        listItemElement.textContent = list[i];
+        unorderedlistelement.append(listItemElement);
+    }
+
 }
-var newtime = new Date();
-var print = greet(prompt("Please enter your name: "), newtime.getHours() + ":" + newtime.getMinutes() + ":" + newtime.getSeconds());
+
+let shoppinglist = ["cheese","bread","green pepper"];
+
+populateList(shoppinglist,prompt());
+
+
